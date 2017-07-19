57e9ef531e5bbc0e7b47cd906b73e0334521767c
Oleg <oleg.pav.m@gmail.com>
2017-07-19 18:49:33 +0300

    Main and some views

---
+++ main.rb
@@ -0,0 +1,42 @@
+require 'recursive-open-struct'
+require 'sinatra'
+
+helpers do
+	def page(path, locale)
+		erb(File.read("views/page/#{path}.html"), locals: {l: locale})
+	end
+
+	def success(data = {})
+		halt 200, {success: true, data: data}.to_json
+	end
+
+	def failure(message='Unknown error')
+		halt 200, {success: false, message: message}.to_json
+	end
+end
+
+locale = RecursiveOpenStruct.new(JSON.parse(File.read('locale/locale.json')))
+
+get '/' do
+	page 'index', locale.en
+end
+
+get '/de' do
+	page 'index', locale.de
+end
+
+get '/fr' do
+	page 'index', locale.fr
+end
+
+get '/ch' do
+	page 'index', locale.ch
+end
+
+get '/ar' do
+	page 'index', locale.ar
+end
+
+get '/ru' do
+	page 'index', locale.ru
+end
