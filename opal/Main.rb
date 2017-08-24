b94b8e12ba995bad5b712baf8a8f428081af5e83
Oleg <oleg.pav.m@gmail.com>
2017-08-24 23:15:31 +0300

    Opal classes stub + some refactoring #2.

--- opal/Main.rb
+++ opal/Main.rb
@@ -2,16 +2,30 @@ require 'opal'
 require 'native'
 require 'opal-jquery'
 
-require 'AllPages'
-require 'RootPage'
-require 'ConsoleSurprise'
+require 'page/AbstractPage'
+require 'page/AllPages'
+require 'page/AboutPage'
+require 'page/Error404Page'
+require 'page/IndexPage'
+require 'page/LoginPage'
+require 'page/ProfilePage'
+require 'page/ProjectPage'
+require 'page/RegisterPage'
+require 'page/RegisterProjectPage'
+require 'util/ConsoleSurprise'
 
 Document.ready? do
 	ConsoleSurprise.new
 	AllPages.new
 
 	case $$[:location].pathname
-		when '/'
-			RootPage.new
+		when /^\//                then IndexPage.new
+		when /^\/about/           then AboutPage.new
+		when /^\/login/           then LoginPage.new
+		when /^\/profile/         then ProfilePage.new
+		when /^\/project/         then ProjectPage.new
+		when /^\/registerProject/ then RegisterProjectPage.new
+		when /^\/register/        then RegisterPage.new
+		else                           Error404Page.new
 	end
 end
