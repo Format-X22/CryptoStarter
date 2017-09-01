0b66c21678929b1105cc013b8f4c7c9933f8a1fa
Oleg <oleg.pav.m@gmail.com>
2017-09-01 11:09:25 +0300

    IdeaCoin.

--- token/IdeaCoin.sol
+++ token/IdeaCoin.sol
@@ -1,54 +1,46 @@
 pragma solidity ^0.4.16;
 
+import 'IdeaBasicCoin';
 import 'IdeaDividendsEngine';
 import 'IdeaProjectsEngine';
+import 'IdeaIco';
 
-contract IdeaCoin is IdeaDividendsEngine, IdeaProjectsEngine {
-    // Public constants
+/**
+ * @notice IdeaCoin (IDEA) - непосредственно сама монета.
+ **/
+contract IdeaCoin is IdeaBasicCoin, IdeaDividendsEngine, IdeaProjectsEngine, IdeaIco {
+
+    /**
+     * @notice Имя монеты.
+     **/
     string public constant name = 'IdeaCoin';
+
+    /**
+     * @notice Аббривеатура монеты.
+     **/
     string public constant symbol = 'IDEA';
-    uint8 public constant decimals = 18;  // 18 decimal places, the same as ETH
-    uint public constant totalSupply = 600000000 * 1000000000000000000; // 600 000 000 tokens
 
-    // Token data
+    /**
+     * @notice Мультипликатор размерности монеты.
+     **/
+    uint8 public constant decimals = 8;
+
+    /**
+     * @notice Общее количество монет.
+     **/
+    uint public constant totalSupply = 600000000 * 100000000; // 600 000 000 IDEA
+
+    /**
+     * @notice Владелец IdeaCoin.
+     **/
     address public owner;
-    mapping(address => uint) internal balances;
 
-    // Events
-    event Transfer(address indexed from, address indexed to, uint value);
-
-    // Constructor
+    /**
+     * @notice Конструктор.
+     **/
     function IdeaCoin() {
         owner = msg.sender;
         balances[owner] = totalSupply;
-    }
-
-    /**
-     * @notice Get IDEA tokens balance for account
-     * @return balance IDEA tokens balance.
-     **/
-    function balanceOf(address target) external constant returns (uint) {
-        return balances[target];
-    }
-
-    /**
-     * @notice Transfer `amount` IDEA tokens from sender's
-     * account `msg.sender` to provided account address `target`.
-     * @param target The address of the tokens recipient
-     * @param amount The amount of token to be transferred
-     * @return Whether the transfer was successful or not
-     **/
-    function transfer(address target, uint amount) public returns (bool) {
-        // We can`t have overflows, no check
-        if (balances[msg.sender] < amount || amount == 0) {
-            return false;
-        }
-
-        balances[msg.sender] -= amount;
-        balances[target] += amount;
-
-        Transfer(msg.sender, target, amount);
-
-        return true;
+        tryCreateAccount(msg.sender);
     }
 }
