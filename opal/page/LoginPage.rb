59565a6afd39f18b58fa78b9f26ca0992fa515fe
Oleg <oleg.pav.m@gmail.com>
2017-09-19 11:19:59 +0300

    Login and register #1.

--- opal/page/LoginPage.rb
+++ opal/page/LoginPage.rb
@@ -1,3 +1,20 @@
 class LoginPage < AbstractPage
-	#
+
+	def initialize
+		map_elements
+		make_handlers
+	end
+
+	def map_elements
+		e = Element
+
+		@email = e['#email']
+		@pass = e['#pass']
+		@login_btn = e['#login']
+	end
+
+	def make_handlers
+		#
+	end
+
 end
--- opal/page/RegisterPage.rb
+++ opal/page/RegisterPage.rb
@@ -1,3 +1,16 @@
 class RegisterPage < AbstractPage
-	#
+
+	def initialize
+		map_elements
+		make_handlers
+	end
+
+	def map_elements
+		#
+	end
+
+	def make_handlers
+		#
+	end
+
 end
