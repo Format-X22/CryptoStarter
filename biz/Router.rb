92f57ee968bb5eeeadfb34124c99b0886e4b0971
Oleg <oleg.pav.m@gmail.com>
2017-09-19 07:46:30 +0300

    Router.

--- biz/Router.rb
+++ biz/Router.rb
@@ -1,1 +1,28 @@
-# Sinatra routing, no class
+get '/'                do page 'index'           end
+get '/about'           do page 'about'           end
+get '/login'           do page 'login'           end
+get '/project'         do page 'project'         end
+get '/register'        do page 'register'        end
+get '/registerProject' do page 'registerProject' end
+get '/restorePass'     do page 'restorePass'     end
+get '/term'            do page 'term'            end
+
+get '/profile'            do inner_page 'profile',            'login' end
+get '/projectConstructor' do inner_page 'projectConstructor', 'login' end
+
+not_found do
+	status 404
+	page 'error404'
+end
+
+post '/api/auth' do
+	# TODO
+end
+
+post '/api/user' do
+	# TODO
+end
+
+post '/api/project' do
+	# TODO
+end
