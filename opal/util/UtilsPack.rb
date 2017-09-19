29ce7d01f010f5d21d5776c4fa77543f3539d8c5
Oleg <oleg.pav.m@gmail.com>
2017-09-19 13:26:38 +0300

    Checkbox validation and passive invalid marker.

--- opal/util/UtilsPack.rb
+++ opal/util/UtilsPack.rb
@@ -11,30 +11,50 @@ module UtilsPack
 
 	def valid_tracker(target, event, &cond)
 		target.on event do
-			if cond.call
-				unmark_invalid target
-			else
-				mark_invalid target
-			end
+			valid_marker target, &cond
+		end
+	end
+
+	def valid_marker(target, &cond)
+		if cond.call
+			unmark_invalid target
+		else
+			mark_invalid target
 		end
 	end
 
 	def mark_invalid(target)
-		target
-			.parent('.form-group')
-			.add_class('has-error')
-			.add_class('has-feedback')
-			.children('.form-control-feedback')
-			.remove_class('hidden')
+		case target.attr('type')
+			when 'checkbox'
+				target
+					.parent('label')
+					.parent('.checkbox')
+					.add_class('has-error')
+			else
+				target
+					.parent('.form-group')
+					.add_class('has-error')
+					.add_class('has-feedback')
+					.children('.form-control-feedback')
+					.remove_class('hidden')
+		end
 	end
 
 	def unmark_invalid(target)
-		target
-			.parent('.form-group')
-			.remove_class('has-error')
-			.remove_class('has-feedback')
-			.children('.form-control-feedback')
-			.add_class('hidden')
+		case target.attr('type')
+			when 'checkbox'
+				target
+					.parent('label')
+					.parent('.checkbox')
+					.remove_class('has-error')
+			else
+				target
+					.parent('.form-group')
+					.remove_class('has-error')
+					.remove_class('has-feedback')
+					.children('.form-control-feedback')
+					.add_class('hidden')
+		end
 	end
 
 	def js_new(func, *args, &block)
