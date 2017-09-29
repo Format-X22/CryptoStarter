227d207d4a056794ec5af67ae3570ae603ca65fd
Oleg <oleg.pav.m@gmail.com>
2017-09-29 22:26:11 +0300

    Profile #3.

--- opal/page/ProfilePage.rb
+++ opal/page/ProfilePage.rb
@@ -14,27 +14,33 @@ class ProfilePage < AbstractPage
 		@edit_security_form = e['#edit-security-form']
 		@create_project_btn = e['#create-project']
 		@edit_projects_btns = e['#projects-table .cs-edit']
+		@main_edit_forms    = e['#main-edit-forms']
+		@body_and_html      = e['body, html']
+		@cancel_about       = e['#cancel-about']
+		@close_security     = e['#close-security']
 	end
 
 	def make_handlers
 		@edit_profile_btn.on :click do
+			@edit_security_form.add_class 'hidden'
+
 			if @edit_profile_form.has_class? 'hidden'
 				@edit_profile_form.remove_class 'hidden'
+				scroll_to_main_edit_form
 			else
 				@edit_profile_form.add_class 'hidden'
 			end
-
-			@edit_security_form.add_class 'hidden'
 		end
 
 		@edit_security_btn.on :click do
+			@edit_profile_form.add_class 'hidden'
+
 			if @edit_security_form.has_class? 'hidden'
 				@edit_security_form.remove_class 'hidden'
+				scroll_to_main_edit_form
 			else
 				@edit_security_form.add_class 'hidden'
 			end
-
-			@edit_profile_form.add_class 'hidden'
 		end
 
 		@create_project_btn.on :click do
@@ -47,6 +53,32 @@ class ProfilePage < AbstractPage
 
 			$$.location.href = "/projectConstructor-#{id}"
 		end
+
+		@cancel_about.on :click do
+			@edit_profile_form.add_class 'hidden'
+			scroll_to_main_edit_controls
+		end
+
+		@close_security.on :click do
+			@edit_security_form.add_class 'hidden'
+			scroll_to_main_edit_controls
+		end
+	end
+
+	def scroll_to_main_edit_controls
+		position = @edit_profile_btn.offset.top - 100
+
+		@body_and_html.animate({scrollTop: position}, 800)
+	end
+
+	def scroll_to_main_edit_form
+		position = @main_edit_forms.offset.top - 100
+
+		@body_and_html.animate({scrollTop: position}, 800)
+	end
+
+	def scroll_to(position)
+		@body_and_html.animate({scrollTop: position}, 800)
 	end
 
 end
